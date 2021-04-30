164398274a5d3db73ea53e5faa44ba39ad3091f7
Marny Lopez <8272737+iMrLopez@users.noreply.github.com>
2021-04-30 15:31:34 -0600

    test 1 - py

---
+++ python/test1.py
@@ -0,0 +1,9 @@
+# Escriba el código para una función de Python llamada expand(x) que toma una lista de strings
+#  las concatena y devuelve la string resultante repetida tres veces.
+
+# ** Ejemplo **
+# Input: ['string1', 'string2']
+# Output: 'string1string2string1string2string1string2'
+
+
+# © Marny Lopez @ 3Pillar Global Costa Rica - https://github.com/iMrLopez
