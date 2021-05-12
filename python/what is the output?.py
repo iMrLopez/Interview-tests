58d2618f4525042a3aafa83548b73447e5b41633
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:29:07 -0600

    Update and rename test2-what is the output?.py to what is the output?.py

--- python/what is the output?.py
+++ python/what is the output?.py
@@ -1,6 +1,7 @@
 # Bienvenido al proceso de seleccion para la posicion de Python!
 # 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
-# 2. Comparta su pantalla con el equipo que le esta entrevistando
+# 2. Comparta su pantalla con el equipo que le esta entrevistando.
+# 3. Explique su solucion de codigo paso a paso para el entrevistador.
 
 d0 = { 'a':1, 'b':2}
 d1 = { 'a':3, 'b':4}
