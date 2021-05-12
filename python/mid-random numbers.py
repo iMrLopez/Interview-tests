fd6c6972b9410a4615bedd4bcdddabb6d8b499a4
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 16:00:59 -0600

    Update mid-random numbers.py

--- python/mid-random numbers.py
+++ python/mid-random numbers.py
@@ -3,4 +3,13 @@
 # 2. Comparta su pantalla con el equipo que le esta entrevistando.
 # 3. Explique su solucion de codigo paso a paso para el entrevistador.
 
+
 # Genere 3 numeros enteros al azar entre 100 y 99 que sean divisibles entre 5
+
+# --> INPUT <---
+
+# n/a
+
+# --> OUTPUT <--
+
+# 3 numeros enteros al azar divisibles entre 5
