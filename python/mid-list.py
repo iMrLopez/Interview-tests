4f2e76e36d8ffc9402ee672aa4eb1ffc55821aeb
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:57:46 -0600

    Update mid-list.py

--- python/mid-list.py
+++ python/mid-list.py
@@ -4,15 +4,16 @@
 # 3. Explique su solucion de codigo paso a paso para el entrevistador.
 
 
-# Dadas dos listas, cree una tercera lista eligiendo todos los elementos de indice impar en la primera lista y los de indice par de la segunda lista y posteriormente ordenandolos de menor a mayor, no deben existir duplicados
+# Dadas dos listas,  extraiga todos los elementos de indice impar en la primera lista y los de indice par de la segunda lista
+# posteriormente, cree una tercera lista ordenandolos de menor a mayor y eliminando cualquier duplicado
 
-# --> Given <---
+# --> INPUT <---
 
 # lista1 = [3, 6, 9, 12, 15, 18, 21]
 # lista2 = [4, 8, 12, 16, 20, 24, 28]
 
 
-# ---> Expected Output <---
+# ---> OUTPUT <---
 
 # Elementos de indice impar
 # [3,6,12,18]
