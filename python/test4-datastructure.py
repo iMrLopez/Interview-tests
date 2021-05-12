8957a4b9745011635750f6af8365ab2ca128629d
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:25:50 -0600

    Create test4-datastructure.py

---
+++ python/test4-datastructure.py
@@ -0,0 +1,10 @@
+# Bienvenido al proceso de seleccion para la posicion de Python!
+# 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
+# 2. Comparta su pantalla con el equipo que le esta entrevistando
+
+
+# Iterate a given list and count the occurrence of each element and create a dictionary to show the count of each element
+# Expected Output:
+
+# Original list  [11, 45, 8, 11, 23, 45, 23, 45, 89]
+# Printing count of each item   {11: 2, 45: 3, 8: 1, 23: 2, 89: 1}
