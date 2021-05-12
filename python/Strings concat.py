5d40b66b046efd3f5b08427b6398d8875eda147c
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:28:53 -0600

    Update and rename test1.py to Strings concat.py

--- python/Strings concat.py
+++ python/Strings concat.py
@@ -1,6 +1,7 @@
 # Bienvenido al proceso de seleccion para la posicion de Python!
 # 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
-# 2. Comparta su pantalla con el equipo que le esta entrevistando
+# 2. Comparta su pantalla con el equipo que le esta entrevistando.
+# 3. Explique su solucion de codigo paso a paso para el entrevistador.
 
 # Escriba el código para una función de Python llamada expand(x) que toma una lista de strings
 #  las concatena y devuelve la string resultante repetida tres veces.
