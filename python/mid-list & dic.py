fefe9fd2eb489774a13e9e8e93130c134df36627
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:59:57 -0600

    Update and rename test4-datastructure.py to mid-list & dic.py

---
+++ python/mid-list & dic.py
@@ -0,0 +1,13 @@
+# Bienvenido al proceso de seleccion para la posicion de Python!
+# 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
+# 2. Comparta su pantalla con el equipo que le esta entrevistando.
+# 3. Explique su solucion de codigo paso a paso para el entrevistador.
+
+
+# Itere una lista dada y cuente la ocurrencia de cada elemento y cree un diccionario para mostrar el recuento de cada elemento
+
+# --> INPUT <--
+# lista = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+
+# --> OUTPUT <--
+# {11: 2, 45: 3, 8: 1, 23: 2, 89: 1}
