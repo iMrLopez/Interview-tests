7c7297e7283a3f036eb2ee3f5f48a0eb67a142f2
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:25:57 -0600

    Update test1.py

--- python/test1.py
+++ python/test1.py
@@ -1,5 +1,6 @@
-# Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
-# Comparta su pantalla con el equipo que le esta entrevistando
+# Bienvenido al proceso de seleccion para la posicion de Python!
+# 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
+# 2. Comparta su pantalla con el equipo que le esta entrevistando
 
 # Escriba el código para una función de Python llamada expand(x) que toma una lista de strings
 #  las concatena y devuelve la string resultante repetida tres veces.
