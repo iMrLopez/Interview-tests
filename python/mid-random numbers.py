34de4b3671fa738b319acdbc4dfa69842378f753
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:48:55 -0600

    Update and rename random numbers.py to mid-random numbers.py

---
+++ python/mid-random numbers.py
@@ -0,0 +1,6 @@
+# Bienvenido al proceso de seleccion para la posicion de Python!
+# 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
+# 2. Comparta su pantalla con el equipo que le esta entrevistando.
+# 3. Explique su solucion de codigo paso a paso para el entrevistador.
+
+# Generate 3 random integers between 100 and 999 which is divisible by 5
--- python/random numbers.py
+++
@@ -1,1 +0,0 @@
-# Generate 3 random integers between 100 and 999 which is divisible by 5
