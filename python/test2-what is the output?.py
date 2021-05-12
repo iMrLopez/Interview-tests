631b8e08999ef60ff15914e4e22177e1a52f4941
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2021-05-12 15:26:16 -0600

    Update and rename test2.py to test2-what is the output?.py

---
+++ python/test2-what is the output?.py
@@ -0,0 +1,7 @@
+# Bienvenido al proceso de seleccion para la posicion de Python!
+# 1. Utilice el interprete en linea en la siguiente direccion https://www.programiz.com/python-programming/online-compiler/
+# 2. Comparta su pantalla con el equipo que le esta entrevistando
+
+d0 = { 'a':1, 'b':2}
+d1 = { 'a':3, 'b':4}
+d1 > d0
--- python/test2.py
+++
@@ -1,5 +0,0 @@
-# cual es la salida del codigo?
-
-d0 = { 'a':1, 'b':2}
-d1 = { 'a':3, 'b':4}
-d1 > d0
